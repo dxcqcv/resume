ef5e15468f4bf0ec3516c01a3ff7f5a26057c19a
dxcqcv <dxcqcv@gmail.com>
2019-02-14 07:42:56 +0800

    Update 2019-02-13T23:42:55.453Z

--- js/index.js
+++ js/index.js
@@ -2,8 +2,15 @@ webpackJsonp([0,1],[
 /* 0 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: \u001b[31mCould not load TypeScript. Try installing with `npm install typescript`. If TypeScript is installed globally, try using `npm link typescript`.\u001b[39m");
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(0);
+
 
 /***/ })
-],[0]);
+],[1]);
 //# sourceMappingURL=index.js.map
