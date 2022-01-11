a29920db0831448e68c0976d2937d74c96c8bf22
dxcqcv <dxcqcv@gmail.com>
2022-01-11 08:52:24 +0800

    feat: update en version comment

--- i18n/en/dist.js
+++ i18n/en/dist.js
@@ -1,16 +1,17 @@
 module.exports = {
   section_name: {
-    "experience": "experience",
-    "education":"education",
-    "projects":"projects",
-    "personal":"personal"
+    experience: "experience",
+    education: "education",
+    projects: "projects",
+    personal: "personal",
   },
   miscellaneous: {
-    "post":"post",
-    "source":"source",
-    "major":"major",
-    "level": "language-level",
-    "selected":"en"
+    post: "post",
+    source: "source",
+    major: "major",
+    level: "language-level",
+    selected: "en",
   },
-  lt_ie8_comment: "你正在使用 IE8 或更老的古董浏览器，还是快升级或者换浏览器吧~"
-}
+  lt_ie8_comment:
+    "You are using IE8 or older browser, please update your browser to Edge or Chrome",
+};
