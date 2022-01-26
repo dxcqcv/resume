5c35338793f00bba9fd7ac1150c4c9e5afd5c275
dxcqcv <dxcqcv@gmail.com>
2022-01-26 15:59:50 +0800

    todo: build pdf no good

--- app/src/js/index.ts
+++ app/src/js/index.ts
@@ -1,3 +1,33 @@
 declare var require: any;
 
-require('../css/index.styl');
+require("../css/index.styl");
+
+/**
+ * download pdf
+ * todo check require from node_modules
+ */
+// const html2canvas = require("./html2canvas.min.js");
+// const { jsPDF } = require("./jspdf.umd.min.js");
+
+// const download = document.querySelector("#download-pdf");
+
+// download.addEventListener("click", () => {
+//   html2canvas(document.querySelector(".container"), { scale: 6 }).then(
+//     (canvas) => {
+//       console.log(canvas);
+//       let pdf = new jsPDF("p", "mm", "a4");
+
+//       const image_height = (211 / canvas.width) * canvas.height;
+//       pdf.addImage(
+//         canvas.toDataURL("image/png"),
+//         "PNG",
+//         0,
+//         0,
+//         211,
+//         image_height,
+//         "FAST"
+//       );
+//       pdf.save("test");
+//     }
+//   );
+// });
